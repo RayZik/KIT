c9e0502d244b221ccd073a706b7a678240fb8234
AlekseyZikrackiy <mralexrabota@gmail.com>
2019-04-21 09:49:49 +0800

    [panel] add react apollo and make the first query

--- panel/src/components/App.tsx
+++ panel/src/components/App.tsx
@@ -1,19 +1,65 @@
 import * as React from "react";
-import "./../assets/css/App.css";
+// import "./../assets/css/App.css";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
 
 export interface AppProps {
+  // data?: {
+  //   document?: {
+  //     get?: {
+  //       id: string;
+  //       name: string;
+  //       description: string;
+  //     }
+  //   }
+  //   loading: boolean;
+  // }
 }
 
-export default class App extends React.Component<AppProps, undefined> {
+class App extends React.Component<any, undefined> {
   render() {
     return (
       <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        <img src={reactLogo} height="480" />
+        {
+          (() => {
+            const data = this.props.data;
+            if (data.loading) {
+              return <div>loading</div>
+            }
+
+            return <div>{data.document.get.id}</div>
+          })()
+        }
       </div>
     );
   }
 }
+
+
+const documentQuery = gql`
+  query ($id: ID!){
+  document {
+    get(id: $id) {
+      id
+      name
+      description
+    }
+  }
+}
+
+`
+
+const AppWithData = graphql(
+  documentQuery,
+  {
+    options: {
+      variables: {
+        id: "1"
+      }
+    }
+  }
+)(App)
+
+export default AppWithData;
--- panel/src/index.tsx
+++ panel/src/index.tsx
@@ -3,14 +3,35 @@ import { render } from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import App from "./components/App";
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 const rootEl = document.getElementById("root");
 
-render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  rootEl
-);
+const httpLink = {
+  uri: 'http://localhost:3001/api'
+};
+
+const client = new ApolloClient({
+  link: new HttpLink(httpLink),
+  cache: new InMemoryCache()
+});
+
+
+const renderWithWrap = (content) => {
+  render(
+    <ApolloProvider client={client}>
+      <AppContainer>
+        {content}
+      </AppContainer>
+    </ApolloProvider>,
+    rootEl
+  )
+}
+
+renderWithWrap(<App />)
 
 // Hot Module Replacement API
 declare let module: { hot: any };
@@ -19,11 +40,6 @@ if (module.hot) {
   module.hot.accept("./components/App", () => {
     const NewApp = require("./components/App").default;
 
-    render(
-      <AppContainer>
-        <NewApp />
-      </AppContainer>,
-      rootEl
-    );
+    renderWithWrap(<NewApp />);
   });
 }
